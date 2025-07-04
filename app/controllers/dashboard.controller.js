f47a19be0367c4a68c0a4ccda656a477124eb5f4
ilhamlutfi <ilhamlutfi153@gmail.com>
2025-07-04 22:33:53 +0700

    feat: skeleton design template

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -3,8 +3,7 @@ import { render } from "../config/view.js";
 class DashboardController {
     async index(req, res) {
         const data = {
-            title: "Beranda",
-            name: "Ilham Lutfi"
+            title: "Cash App",
         };
 
         return render(req, res, 'dashboard/index', data);
